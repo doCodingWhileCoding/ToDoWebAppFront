7ea52fc261a9f34e11e70f874e20dd3208f74192
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-09-21 23:36:06 +0200

    added react-query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './main.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
