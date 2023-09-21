74d6f803ac0c333009a9425020755a497a437fad
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-09-21 23:40:41 +0200

    added react-query-devtools

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './main.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 )
