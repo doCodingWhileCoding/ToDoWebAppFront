856a9aa50db91056b7a410fe5898cd1865bb198a
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-12-03 18:53:50 +0100

    solved an error in the date display format

--- src/components/EditTaskDate.jsx
+++ src/components/EditTaskDate.jsx
@@ -1,12 +1,11 @@
 import '../assets/scss/edittaskdate.scss'
 import PropTypes from 'prop-types'
-import { addDays, isAfter, isSameWeek, isToday, isTomorrow, format, startOfToday } from 'date-fns'
+import { addDays, isAfter, isSameWeek, isToday, isTomorrow, format, startOfToday, isEqual } from 'date-fns'
 import { useEditTaskDateModalStore } from '../store/app.store'
 import { es } from 'date-fns/locale'
 import CalendarIcon from '../assets/icons/CalendarIcon'
 const EditTaskDate = (props) => {
   const { date } = props
-  console.log(date)
   const setShowEditTaskDateModal = useEditTaskDateModalStore((state) => state.setShowEditTaskDateModal)
   const today = startOfToday()
   return (
@@ -15,8 +14,9 @@ const EditTaskDate = (props) => {
       <div className="text">
         {isToday(date) && <p>{'Hoy'}</p>}
         {isTomorrow(date) && <p>{'Mañana'}</p>}
-        {isAfter(date, addDays(today, 2)) && isSameWeek(date, today) && <p>{format(date, 'EEEE', { locale: es })}</p>}
-        {!isSameWeek(date, today) && <p>{format(date, 'EEE, d MMM', { locale: es })}</p>}
+        {(isEqual(date, addDays(today, 2)) || isAfter(date, addDays(today, 2))) &&
+          isSameWeek(date, today, { weekStartsOn: 1 }) && <p>{format(date, 'EEEE', { locale: es })}</p>}
+        {!isSameWeek(date, today, { weekStartsOn: 1 }) && <p>{format(date, 'EEE, d MMM', { locale: es })}</p>}
       </div>
     </div>
   )
