dc069d21259c5cba4855276574005f372b7714b2
doCodingWhileCoding <nadirboughlalajob@gmail.com>
2023-12-03 18:52:33 +0100

    some minimun style changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -17,6 +16,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve('./src'),
-    }
-  }
+    },
+  },
 })
